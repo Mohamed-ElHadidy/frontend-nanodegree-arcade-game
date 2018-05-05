cc53d8c0bf45ef4b9d5b6a700f8c4f45a8b7702d
Muhammed El-Hadidy <mohammed.elhadidy90@gmail.com>
2018-05-05 13:15:55 +0200

    Fix: fix the errors

--- js/app.js
+++ js/app.js
@@ -25,13 +25,25 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+const Player = function(x, y, sprite) {
+
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+};
+Player.prototype.update = function() {};
+Player.prototype.render = function() {};
+Player.prototype.handleInput = function() {};
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
-
+const allEnemies = [];
+const player = new Player(50, 400);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
