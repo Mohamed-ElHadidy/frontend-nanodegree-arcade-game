7d0ed0d16331bd315869b67800d679e845b8f8e5
Muhammed El-Hadidy <mohammed.elhadidy90@gmail.com>
2018-05-05 15:08:59 +0200

    feat: added 3 enemies

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, sprite) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y; 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -41,8 +42,10 @@ Player.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-const allEnemies = [];
+const enemy1 = new Enemy(300, 140);
+const enemy2 = new Enemy(3, 225);
+const enemy3 = new Enemy(100, 60);
+const allEnemies = [enemy1, enemy2, enemy3];
 const player = new Player(50, 400);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
